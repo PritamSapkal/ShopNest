fc2149b58c8dee0eb6d63cef7d18f59359401d1d
PritamSapkal <pritam.s0752@gmail.com>
2026-09-21 23:23:33 +0530

    Add the hive and update teh all the function like update , delete nad add th new item and toggel status and allow to store data locally

--- shopnest/lib/Provider/MasterItemList.dart
+++ shopnest/lib/Provider/MasterItemList.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:shopnest/DataModel/ItemModel.dart';
+import 'package:hive_ce/hive.dart';
 
 class Masteritemlist extends StateNotifier<List<Itemmodel>> {
-  Masteritemlist() : super([]);
 
+  final Box<Itemmodel> _itemsBox;// itembox
+  Masteritemlist({required this._itemsBox}) : super(_itemsBox.values.toList());
+// first reads teh all the avalable value in the box means  in the local storage
   void addItem(Itemmodel item) {
+
     state = [...state, item];
+
+    _itemsBox.put(item.id, item);
+
   }
 
   void deleteItemById(String id) {
     state = state.where((item) => item.id != id).toList();
+    _itemsBox.delete(id);
   }
 
   void updateItem(Itemmodel updatedItem) {
@@ -17,20 +25,30 @@ class Masteritemlist extends StateNotifier<List<Itemmodel>> {
       for (final item in state)
         if (item.id == updatedItem.id) updatedItem else item,
     ];
+    _itemsBox.put(updatedItem.id, updatedItem);
   }
 
   void toggleStatus(String id) {
-    state = [
+    final updatedList = [
       for (final item in state)
         if (item.id == id)
-          item.copyWith(status: !item.status,)
+          item.copyWith(status: !item.status)
         else
           item,
     ];
+
+    state = updatedList;
+
+    final updatedItem = updatedList.firstWhere(
+          (item) => item.id == id,
+    );
+
+    _itemsBox.put(id, updatedItem);
   }
 }
 
 final masteritemlistProvider =
 StateNotifierProvider<Masteritemlist, List<Itemmodel>>((ref) {
-  return Masteritemlist();
+  final box = Hive.box<Itemmodel>('items');
+  return Masteritemlist(itemsBox: box);
 });
