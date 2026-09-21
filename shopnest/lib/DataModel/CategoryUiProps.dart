5e0b7295dea620387411ca1652a6aba27885a13f
PritamSapkal <pritam.s0752@gmail.com>
2026-09-21 20:05:01 +0530

    generate the itemModel and CategoryUiProps Adpter classes usin teh hive_Generator ,successfully

--- shopnest/lib/DataModel/CategoryUiProps.dart
+++ shopnest/lib/DataModel/CategoryUiProps.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:hive_ce/hive.dart';
 
+part 'CategoryUiProps.g.dart';
 enum ItemCategory {
   all,
   vegetables,
@@ -15,10 +17,20 @@ enum ItemCategory {
   healthAndCare,      // Medicines, Baby care, Shampoos, Personal hygiene
   others              // Catch-all fallback
 }
+
+@HiveType(typeId: 1)
 class CategoryUiProps {
   CategoryUiProps({required this.name,required this.icon,required this.baseColor,required this.backgroundColor});
+
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final IconData icon;
+
+  @HiveField(2)
   final Color baseColor;
+
+  @HiveField(3)
   final Color backgroundColor;
 }
--- shopnest/lib/DataModel/ItemModel.dart
+++ shopnest/lib/DataModel/ItemModel.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'CategoryUiProps.dart';
+import 'package:hive_ce/hive.dart';
+part 'ItemModel.g.dart';
 
+@HiveType(typeId: 0)
 class Itemmodel {
   Itemmodel({
     required this.name,
@@ -13,18 +15,31 @@ class Itemmodel {
     String? id,
   }) : id = id ?? const Uuid().v4();
 
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final CategoryUiProps category;
+
+  @HiveField(3)
   final String quantity;
+
+  @HiveField(4)
   final String notes;
+
+  @HiveField(5)
   final bool status;
+
+  @HiveField(6)
   final DateTime  addedTime;
 
   Itemmodel copyWith({
     String? name,
     CategoryUiProps? category,
-    required String quantity,
+    String? quantity,
     String? notes,
     bool? status,
     DateTime ? addedTime,
