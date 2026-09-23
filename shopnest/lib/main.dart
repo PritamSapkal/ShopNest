88a4fff39628682d86428f1829bd2991b6f0f749
PritamSapkal <pritam.s0752@gmail.com>
2026-09-23 19:25:39 +0530

    remov aunecessary prin statments

--- shopnest/lib/main.dart
+++ shopnest/lib/main.dart
@@ -21,13 +21,6 @@ void main()async{
    await Hive.openBox<Itemmodel>('items');
 
   final box = Hive.box<Itemmodel>('items');
-  print('==============================');
-  print('MAIN HIVE BOX');
-  print('Box path: ${box.path}');
-  print('Box length: ${box.length}');
-  print('Box keys: ${box.keys.toList()}');
-  print('Box values: ${box.values.map((e) => e.name).toList()}');
-  print('==============================');
    return runApp(ProviderScope(child: MyApp()));
 }
 class MyApp extends ConsumerWidget {
