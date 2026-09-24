5c04f9665fe39129d788de0440bbacf344449e83
PritamSapkal <pritam.s0752@gmail.com>
2026-09-24 23:35:25 +0530

    Delete the unnecessary data madel

--- shopnest/lib/DataModel/SpecificCVategoryLIstCla.dart
+++
@@ -1,7 +0,0 @@
-import 'ItemModel.dart';
-
-class Specificcvategorylistcla {
-  Specificcvategorylistcla({required this.filteredList,required this.TotalItem});
-final List<Itemmodel> filteredList;
-final int TotalItem;
-}
